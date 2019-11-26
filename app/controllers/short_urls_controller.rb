0e87e3a83056177fe0d893de0b0d5e3acb01ef39
Russel <russel.tsang@gmail.com>
2019-11-26 16:52:17 -0500

    add logic for post request

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -4,9 +4,17 @@ class ShortUrlsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
+    # @short_urls = ShortUrl.order(click_count: :desc).limit(100)
+    # render :index
   end
 
   def create
+    short_url = ShortUrl.new(full_url: params[:full_url])
+    if short_url.valid?
+      render json: {short_code: short_url.short_code}
+    else
+      render json: @short_url.errors.full_messages, status: 422
+    end
   end
 
   def show
