f40e23f49146193e916772fab99d98109a6906c2
Russel <russel.tsang@gmail.com>
2019-11-28 16:36:12 -0500

    create short_urls table with constraints

--- db/migrate/20190923133432_create_short_urls.rb
+++ db/migrate/20190923133432_create_short_urls.rb
@@ -1,11 +1,12 @@
 class CreateShortUrls < ActiveRecord::Migration[6.0]
   def change
     create_table :short_urls do |t|
-      t.string :short_urls, :title, 
-      t.string :short_urls, :full_url, null: false
-      t.string :short_urls, :short_code, 
-      t.integer :short_urls, :click_count, default: 0, null: false,
+      t.string :title 
+      t.string :full_url, null: false
+      t.string :short_code 
+      t.integer :click_count, default: 0
       t.timestamps
     end
+    add_index :short_urls, :full_url, :unique => true
   end
 end
