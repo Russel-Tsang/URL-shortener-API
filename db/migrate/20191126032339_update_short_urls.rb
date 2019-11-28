7f51a9a18e3b71088f78fba99c4e30b3b88eaaf2
Russel <russel.tsang@gmail.com>
2019-11-28 15:00:18 -0500

    clean migration files

--- db/migrate/20191126032339_update_short_urls.rb
+++
@@ -1,8 +0,0 @@
-class UpdateShortUrls < ActiveRecord::Migration[6.0]
-  def change
-    add_column :short_urls, :title, :string, null: false
-    add_column :short_urls, :full_url, :string, null: false
-    add_column :short_urls, :short_code, :string, null: false
-    add_column :short_urls, :click_count, :integer, null: false, default: 0
-  end
-end
