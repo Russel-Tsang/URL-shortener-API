b6784f001829af1173e641daf40a9322862f249c
Russel <russel.tsang@gmail.com>
2019-11-28 14:59:14 -0500

    add migration for creating short_urls table

--- db/migrate/20190923133432_create_short_urls.rb
+++ db/migrate/20190923133432_create_short_urls.rb
@@ -1,8 +1,10 @@
 class CreateShortUrls < ActiveRecord::Migration[6.0]
   def change
     create_table :short_urls do |t|
-      # You'll want to add some attributes here...
-
+      t.string :short_urls, :title, 
+      t.string :short_urls, :full_url, null: false
+      t.string :short_urls, :short_code, 
+      t.integer :short_urls, :click_count, default: 0, null: false,
       t.timestamps
     end
   end
